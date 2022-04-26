b22b6cc06e9b61b0f7ad7128848aec6075ab95f1
matyuninatatiana <matyuninatatiana@gmail.com>
2022-04-26 21:42:10 +0300

    Add task solution

---
+++ project_0/game_v3.py
@@ -0,0 +1,53 @@
+"""Игра угадай число
+Компьютер сам загадывает и сам угадывает число
+"""
+
+import numpy as np
+
+def optimal_predict(number: int = 1) -> int:
+    '''Игра компьютер угадает число меньше чем за 20 попыток'''
+
+    min = 1
+    max = 101
+
+    number = np.random.randint(min, max)
+
+    count = 0
+
+    while True:
+        count+=1
+        mid = (min+max) // 2
+        
+        if mid > number:
+            max = mid
+            
+        elif mid < number:
+            min = mid
+
+        else:
+            #print(f"Компьютер угадал число за {count} попыток. Это число {number}")
+            break #конец игры выход из цикла
+    return count
+
+def score_game(optimal_predict) -> int:
+    """Какое количество попыток в среднем за 1000 подходов угадывает наш алгоритм
+
+    Args:
+        optimal_predict ([type]): функция угадывания
+
+    Returns:
+        int: среднее количество попыток
+    """
+    count_ls = [] #список для сохранения количества попыток
+    np.random.seed(1) # фиксируем сид для воспроизводимости
+    random_array = np.random.randint(1, 101, size=(1000)) # загадали список чисел
+
+    for number in random_array:
+        count_ls.append(optimal_predict(number))
+
+        score = int(np.mean(count_ls))
+        print(f"Ваш алгоритм угадывает число в среднем за:{score} попыток")
+        return score
+
+
+score_game(optimal_predict)
